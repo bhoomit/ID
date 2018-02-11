c3f6b13f3fc018206089b1fe43fa14b36f91167f
bhoomit <bhoomit.2010@gmail.com>
2018-02-11 10:28:12 +0530

    converted to bytes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Python 2
+```
 > virtualenv env -p python2.7
 > . env/bin/activate
 > pip install -r requirments.txt
+```
--- rsa.py
+++ rsa.py
@@ -5,7 +5,7 @@ def random_org_str(n):
     print(n)
     url = "https://www.random.org/strings/?num={0}&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new"
     response = requests.get(url.format((n/20) + 1))
-    return ''.join(response.text.split('\n'))[:n]
+    return ''.join(response.text.split('\n'))[:n].encode('utf-8')
 
 bits = 2048
 new_key = RSA.generate(bits, e=65537, randfunc=random_org_str)
@@ -13,4 +13,3 @@ publicKey = new_key.publickey().exportKey("PEM")
 privateKey = new_key.exportKey("PEM")
 print("Public Key: ", publicKey)
 print("Private Key: ", privateKey)
-tqeCrodJ2F6sJfp1BUW6
